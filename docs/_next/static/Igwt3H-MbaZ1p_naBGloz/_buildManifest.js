e774d26a7fbad8a593ff1d6fbc1af4102962de33
carlosjasso <contact@carlosjasso.dev>
2020-08-28 17:18:23 -0500

    I'm giving up on ghpages deployment

---
+++ docs/_next/static/Igwt3H-MbaZ1p_naBGloz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002F5d5be768c089998a471a.css","static\u002Fchunks\u002Fpages\u002Findex-b32632865223b5e67352.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"],"/quote":["static\u002Fchunks\u002Fpages\u002Fquote-8d4699b720def910ff4a.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? '/fcc-front-end-libraries' : '/',
+  assetPrefix: isProd ? '/fcc-front-end-libraries/' : '/',
   basePath: isProd ? '/fcc-front-end-libraries' : '/'
 }
