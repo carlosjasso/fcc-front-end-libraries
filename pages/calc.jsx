91e2f3dd8082cd36141531cc2c4ce63372da6806
carlosjasso <contact@carlosjasso.dev>
2020-10-06 22:15:06 -0500

    attached click event handler

--- pages/calc.jsx
+++ pages/calc.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Styles from "@styles/calc.module.scss";
 import AppendFCCScript from "@lib/appendFCCScript";
+import { types } from "react-markdown";
 
 const buttonType = {
     numeric: 0,
@@ -115,25 +116,6 @@ const buttons = {
     }
 }
 
-function Button(button) {
-    const { button: { name, symbol, type }  } = button;
-    let style = "";
-    switch (type) {
-        case buttonType.numeric: style = "numeric"; break;
-        case buttonType.operator: style = "operator"; break;
-        case buttonType.result: style = "result"; break;
-        case buttonType.clear: style = "clear"; break;
-        case buttonType.interface: style = "interface"; break;
-        case buttonType.extra: style = "extra"; break;
-    }
-
-    return (
-        <button id={name} className={Styles[style]}>
-            {symbol}
-        </button>
-    );
-}
-
 function Screen() {
     return (
         <section id="display" className={Styles["display"]}>
@@ -147,28 +129,79 @@ function Screen() {
     );
 }
 
-function Pad() {
-    const order = [
-        16, 17, 18, 19,
-        7,  8,  9,  14,
-        4,  5,  6,  13,
-        1,  2,  3,  12,
-        10, 0,  15, 11
-    ];
-    const pad = order.map(id => {
-        return <Button button={buttons[id]} key={id} />
-    });
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <section className={Styles["pad"]}>
-            {pad.map(button => button)}
-        </section>
-    );
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    // Event handlers
+    handleClick() {
+        this.props.clickHandler(this.props.button);
+    }
+    
+    render() {
+        const { name, symbol, type } = this.props.button;
+        let style = "";
+        switch (type) {
+            case buttonType.numeric: style = "numeric"; break;
+            case buttonType.operator: style = "operator"; break;
+            case buttonType.result: style = "result"; break;
+            case buttonType.clear: style = "clear"; break;
+            case buttonType.interface: style = "interface"; break;
+            case buttonType.extra: style = "extra"; break;
+        }
+        
+        return (
+            <button id={name} className={Styles[style]} onClick={this.handleClick} >
+                {symbol}
+            </button>
+        );
+    }
+}
+
+class Pad extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    // Event handlers
+    handleClick(button) {
+        this.props.clickHandler(button);
+    }
+
+    render() {
+        const order = [
+            16, 17, 18, 19,
+            7,  8,  9,  14,
+            4,  5,  6,  13,
+            1,  2,  3,  12,
+            10, 0,  15, 11
+        ];
+        const pad = order.map(id => {
+            return (
+                <Button 
+                button={buttons[id]} 
+                clickHandler={this.handleClick}
+                key={id} />
+            );
+        });
+    
+        return (
+            <section className={Styles["pad"]}>
+                {pad.map(button => button)}
+            </section>
+        );
+    }
 }
 
 export default class Calculator extends React.Component {
     constructor(props) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     // Element lifecycle
@@ -176,9 +209,12 @@ export default class Calculator extends React.Component {
         AppendFCCScript();
     }
 
-    render() {
-        
+    // Event handlers
+    handleClick(button) {
+        console.log(button);
+    }
 
+    render() {
         return (
             <main className={Styles["page-container"]}>
                 <Head>
@@ -187,7 +223,7 @@ export default class Calculator extends React.Component {
                 <img className={Styles["wallpaper"]} />
                 <section className={Styles["calculator"]}>
                     <Screen />
-                    <Pad />
+                    <Pad clickHandler={this.handleClick} />
                 </section>
             </main>
         );
