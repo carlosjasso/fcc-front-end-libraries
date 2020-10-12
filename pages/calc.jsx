8d03d4e6871a4a44e1a00f6b227b6055a2cd49c0
Carlos Jasso <contact@carlosjasso.dev>
2020-10-12 17:02:49 -0500

    Almost done. Logic for negative numbers still in progress

--- pages/calc.jsx
+++ pages/calc.jsx
@@ -5,124 +5,129 @@ import AppendFCCScript from "@lib/appendFCCScript";
 
 const buttonType = {
     numeric: 0,
-    operator: 1,
-    result: 2,
-    clear: 3,
+    clear: 1,
+    operator: 2,
+    equals: 3,
     interface: 4,
     extra: 5
-}
+};
 
-const buttons = {
-    0: {
+const buttons = [
+    {
         name: "zero",
         value: "0",
         type: buttonType.numeric
     },
-    1: {
+    {
         name: "one",
         value: "1",
         type: buttonType.numeric
     },
-    2: {
+    {
         name: "two",
         value: "2",
         type: buttonType.numeric
     },
-    3: {
+    {
         name: "three",
         value: "3",
         type: buttonType.numeric
     },
-    4: {
+    {
         name: "four",
         value: "4",
         type: buttonType.numeric
     },
-    5: {
+    {
         name: "five",
         value: "5",
         type: buttonType.numeric
     },
-    6: {
+    {
         name: "six",
         value: "6",
         type: buttonType.numeric
     },
-    7: {
+    {
         name: "seven",
         value: "7",
         type: buttonType.numeric
     },
-    8: {
+    {
         name: "eight",
         value: "8",
         type: buttonType.numeric
     },
-    9: {
+    {
         name: "nine",
         value: "9",
         type: buttonType.numeric
     },
-    10: {
+    {
         name: "decimal",
         value: ".",
         type: buttonType.numeric
     },
-    11: {
+    {
         name: "add",
         value: "+",
         type: buttonType.operator
     },
-    12: {
+    {
         name: "subtract",
         value: "-",
         type: buttonType.operator
     },
-    13: {
+    {
         name: "multiply",
         value: "*",
         type: buttonType.operator
     },
-    14: {
+    {
         name: "divide",
         value: "/",
         type: buttonType.operator
     },
-    15: {
+    {
         name: "equals",
         value: "=",
-        type: buttonType.result
+        type: buttonType.equals
     },
-    16: {
+    {
         name: "clear",
         value: "AC",
         type: buttonType.clear
     },
-    17: {
+    {
         name: "cancel",
         value: "C",
         type: buttonType.interface
     },
-    18: {
+    {
         name: "delete",
         value: "Del",
         type: buttonType.interface
     },
-    19: {
+    {
         name: "github",
         value: "GH",
         type: buttonType.extra
     }
-}
+];
 
-function Screen({ history, input }) {
+function Screen({ history, input, positive }) {
     return (
         <section className={Styles["display"]}>
             <div className={Styles["history"]}>
                 {history}
             </div>
-            <div id="display" className={Styles["input"]}>
-                {input}
+            <div className={Styles["bottom"]}>
+                {!positive && <div className={Styles["negative"]}>
+                    -
+                </div>}
+                <div id="display" className={Styles["input"]}>
+                    {input}
+                </div>
             </div>
         </section>
     );
@@ -146,7 +151,7 @@ class Button extends React.Component {
         switch (type) {
             case buttonType.numeric: style = "numeric"; break;
             case buttonType.operator: style = "operator"; break;
-            case buttonType.result: style = "result"; break;
+            case buttonType.equals: style = "result"; break;
             case buttonType.clear: style = "clear"; break;
             case buttonType.interface: style = "interface"; break;
             case buttonType.extra: style = "extra"; break;
@@ -173,19 +178,20 @@ class Pad extends React.Component {
     }
 
     render() {
-        const order = [
-            16, 17, 18, 19,
-            7,  8,  9,  14,
-            4,  5,  6,  13,
-            1,  2,  3,  12,
-            10, 0,  15, 11
+        const padOrder = [
+            "AC", "C", "Del", "GH",
+            "7",  "8",  "9",  "/",
+            "4",  "5",  "6",  "*",
+            "1",  "2",  "3",  "-",
+            ".",  "0",  "=",  "+"
         ];
-        const pad = order.map(id => {
+        const pad = padOrder.map(value => {
+            const padButton = buttons.filter(button => button.value == value)[0];
             return (
                 <Button 
-                button={buttons[id]} 
+                button={padButton} 
                 clickHandler={this.handleClick}
-                key={id} />
+                key={padButton.value} />
             );
         });
     
@@ -205,7 +211,9 @@ export default class Calculator extends React.Component {
         this.state = {
             history: "0",
             input: "0",
+            cleared: true,
             lastOperator: null,
+            positive: true,
             result: 0
         }
     }
@@ -221,58 +229,46 @@ export default class Calculator extends React.Component {
             case buttonType.numeric: this.handleNumericButton(button); break;
             case buttonType.operator: this.handleOperatorButton(button); break;
             case buttonType.clear: this.handleClearButton(); break;
+            case buttonType.equals: this.handleEqualsButton(button); break;
         }
     }
 
     // Util Functions
     handleNumericButton(button) {
         const input = this.state.input;
-        let inputUpdate = "";
-        if (input == "0") {
+        let stateUpdate = this.state;
+        if (this.state.cleared) {
             switch(button.value) {
-                case "0" : 
-                    inputUpdate = button.value;
-                    break
                 case ".":
-                    inputUpdate = "0.";
+                    stateUpdate.input = "0.";
+                    stateUpdate.cleared = false;
                     break;
-                default: 
-                    inputUpdate = button.value;
+                case "0":
+                    stateUpdate.input = button.value;
+                    stateUpdate.cleared = true;
                     break;
-            }
+                default:
+                    stateUpdate.input = button.value;
+                    stateUpdate.cleared = false;
+                    break;
+            };
         } else {
             switch(button.value) {
-                case ".": 
-                    if (input.includes(".")) {
-                        inputUpdate = input;
+                case ".":
+                    stateUpdate.input = input.includes(".") ? input : `${input}${button.value}`;
+                    break;
+                case "0":
+                    if (input == 0) {
+                        stateUpdate.input = button.value;
+                        stateUpdate.cleared = true;
                     } else {
-                        inputUpdate = `${input}${button.value}`;
+                        stateUpdate.input = `${input}${button.value}`;
                     }
                     break;
                 default: 
-                    inputUpdate = `${input}${button.value}`;
+                    stateUpdate.input = `${input}${button.value}`;
                     break;
-            }
-        }
-
-        this.setState({
-            input: inputUpdate
-        });
-    }
-
-    handleOperatorButton(button) {
-        // 11 +
-        // 12 -
-        // 13 *
-        // 14 /
-        const history = this.state.history;
-        const input = this.state.input;
-        let stateUpdate = this.state;
-        stateUpdate.input = "0";
-        if (history == "0") {
-            stateUpdate.history = `${input}${button.value}`;
-        } else {
-            stateUpdate.history = `${history}${input}${button.value}`;
+            };
         }
 
         this.setState(stateUpdate);
@@ -282,9 +278,73 @@ export default class Calculator extends React.Component {
         this.setState({
             history: "0",
             input: "0",
-            nextOperation: null,
+            cleared: true,
+            lastOperator: null,
             result: 0
         });
+    }
+
+    handleOperatorButton(button) {
+        const history = this.state.history;
+        const input = this.state.input;
+        const numericInput = this.getNumericValue(input);
+        const operators = buttons.filter(button => button.type == buttonType.operator);
+        const substractButton = buttons.filter(button => button.value == "-")[0];
+        let stateUpdate = this.state;
+
+        if (this.state.lastOperator == null) {
+            stateUpdate.lastOperator = button;
+            stateUpdate.history = `${input}${button.value}`;
+            stateUpdate.input = "0";
+            stateUpdate.cleared = true;
+            stateUpdate.result = numericInput;
+        } else if(button == substractButton) {
+            stateUpdate.positive = !this.state.positive;
+        } else {
+            const lastChar = history.slice(-1);
+            const isLastCharOperator = operators.filter(button => button.value == lastChar).length > 0;
+            const isNotEquals = button.type != buttonType.equals;
+            if (this.state.cleared && isLastCharOperator && isNotEquals) {
+                stateUpdate.lastOperator = button;
+                stateUpdate.history = isNotEquals ? `${history.slice(0, -1)}${button.value}` : `${history.slice(0, -1)}${button.value}${this.state.result}`;
+            } else {
+                switch(this.state.lastOperator.value) {
+                    case "+":
+                        stateUpdate.result = this.state.result + numericInput;
+                        break;
+                    case "-":
+                        stateUpdate.result = this.state.result - numericInput;
+                        break;
+                    case "*":
+                        stateUpdate.result = this.state.result * numericInput;
+                        break;
+                    case "/":
+                        stateUpdate.result = this.state.result / numericInput;
+                        break;
+                }
+                if (isNotEquals) {
+                    stateUpdate.history = `${history}${input}${button.value}`;
+                    stateUpdate.input = `${stateUpdate.result}`;
+                    stateUpdate.cleared = true;
+                    stateUpdate.lastOperator = button;
+                } else {
+                    stateUpdate.history = `${history}${input}${button.value}${this.state.result}`;
+                    stateUpdate.input = `${stateUpdate.result}`;
+                    stateUpdate.cleared = true;
+                    stateUpdate.lastOperator = null;
+                }
+            }
+        }
+
+        this.setState(stateUpdate);
+    }
+
+    handleEqualsButton(button) {
+        if (this.state.lastOperator == null) {
+            return;
+        } else {
+            this.handleOperatorButton(button);
+        }
     }
 
     getNumericValue(value) {
@@ -305,7 +365,10 @@ export default class Calculator extends React.Component {
                 </Head>
                 <img className={Styles["wallpaper"]} />
                 <section className={Styles["calculator"]}>
-                    <Screen history={this.state.history} input={this.state.input} />
+                    <Screen 
+                    history={this.state.history} 
+                    input={this.state.input} 
+                    positive={this.state.positive} />
                     <Pad clickHandler={this.handleClick} />
                 </section>
             </main>
